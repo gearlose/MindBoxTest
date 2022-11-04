ccffbecd08e4c8e033607d62aed7a6e183572071
gearlose <kirilmorozov149@yandex.ru>
2022-11-04 18:30:47 +0300

    FixSyntaxMistake

--- CalcAreaLib/CalcAreaLib.cs
+++ CalcAreaLib/CalcAreaLib.cs
@@ -2,14 +2,14 @@
 
 namespace CalcAreaLib
 {
-    public class CalcAreaLib
+    public class CalcAreaLib : ICalculatingArea
     {
         public double CircleCalcArea(double Radius)
         {
             return Math.Round((Math.PI * Math.Pow(Radius, 2)),2);
         }
 
-        public double TriangleCalcArea(double A, double B, double C)
+        public double TriangleCricleArea(double A, double B, double C)
         {
             double p = (A + B + C) / 2;
             if ((A * A + B * B == C * C) || (A * A + C * C == B * B) || (C * C + B * B == A * A))
